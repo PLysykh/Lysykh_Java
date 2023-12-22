720834228c9a06f68f35c3366dd9dd125b5ae037
Pavel Lysykh <bourne33@yandex.ru>
2023-12-22 12:57:25 +0300

    first commit added

---
+++ src/main/java/org/example/NameCheck.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NameCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = scanner.nextLine();
+
+        if (name.equals("Вячеслав")) {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+        scanner.close();
+    }
+}
---
+++ src/main/java/org/example/NumberCheck.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        if (number > 7) {
+            System.out.println("Привет");
+        }
+        scanner.close();
+    }
+}
